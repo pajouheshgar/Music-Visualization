a1ef39a7f1f11438b9f5df3875834412d85aa373
Ehsan Pajouheshgar <pajouheshgar@cafebazaar.ir>
2020-03-11 12:45:31 +0330

    Add mp3 to wav conversion

---
+++ mp3towav.py
@@ -0,0 +1,12 @@
+import os
+import sys
+import subprocess
+
+if __name__ == '__main__':
+    mp3_dir = sys.argv[1]
+    wav_dir = sys.argv[2]
+    mp3_files = os.listdir(mp3_dir)
+    for mp3_file in mp3_files:
+        wav_file_name = mp3_file.replace("mp3", "wav")
+        convert_command = f"mpg123 -w {os.path.join(wav_dir, wav_file_name)} {os.path.join(mp3_dir, mp3_file)}"
+        subprocess.call(convert_command.split())
