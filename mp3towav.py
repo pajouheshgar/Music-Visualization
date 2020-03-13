a4334f60bf58f5b8ab15406c9858b383e62661cb
seyed <moosavia2@gmail.com>
2020-03-14 01:18:25 +0330

    Fix space in sample name bug

--- mp3towav.py
+++ mp3towav.py
@@ -8,5 +8,5 @@ if __name__ == '__main__':
     mp3_files = os.listdir(mp3_dir)
     for mp3_file in mp3_files:
         wav_file_name = mp3_file.replace("mp3", "wav")
-        convert_command = f"mpg123 -w {os.path.join(wav_dir, wav_file_name)} {os.path.join(mp3_dir, mp3_file)}"
-        subprocess.call(convert_command.split())
+        convert_command = ["mpg123", "-w", os.path.join(wav_dir, wav_file_name), os.path.join(mp3_dir, mp3_file)]
+        subprocess.call(convert_command)
